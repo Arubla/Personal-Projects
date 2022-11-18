45cef35aed60a363a7fdf0a7bd9ed0e618414102
arubla <arunblatchley@gmail.com>
2022-11-17 21:48:28 -0500

    started button project

---
+++ button-chase/js/app.js
@@ -0,0 +1,8 @@
+const buttonMocks = ["Too slow!",
+                    "That all you got?",
+                    "I could dodge you in my sleep!",
+                    "C'mon, try harder!",
+                    "Give it another try!",
+                    "Do you need a new mouse?",
+                    "Try clicking faster"
+                    ];
