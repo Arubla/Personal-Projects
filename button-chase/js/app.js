de18a15e59f5b4595d942282718f5ab35ccc601c
arubla <105884760+Arubla@users.noreply.github.com>
2022-11-26 21:20:30 -0500

    Delete button-chase directory

--- button-chase/js/app.js
+++
@@ -1,8 +0,0 @@
-const buttonMocks = ["Too slow!",
-                    "That all you got?",
-                    "I could dodge you in my sleep!",
-                    "C'mon, try harder!",
-                    "Give it another try!",
-                    "Do you need a new mouse?",
-                    "Try clicking faster"
-                    ];
